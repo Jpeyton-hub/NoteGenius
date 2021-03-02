db51af84b4c2e466522cdfd07958654d14e04da6
Jpeyton <jpwalk319@gmail.com>
2021-03-01 19:57:42 -0700

    set static file to public and correct href in index.html for the notes page

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
+const fs = require('fs');
+const uuid = require('uuid');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static('public'));
+
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
   });
+
+  
