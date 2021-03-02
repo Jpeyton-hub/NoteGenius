2bd24351deb9f62bce77e193f64d54c12edc713d
Jpeyton <jpwalk319@gmail.com>
2021-03-01 20:27:24 -0700

    added get route to indexjs and adiusted some of the functions in js/index.js so they will recognize the correct path

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const PORT = process.env.PORT || 8080;
 const fs = require('fs');
 const uuid = require('uuid');
 
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.use(express.static('public'));
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
   });
+
+app.get('/api/notes', (req, res) => {
+    res.json('./db/db.json');
+})  
 
   
